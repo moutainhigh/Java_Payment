8b18da80bc7b9349cc3157011e39e0b99645fa9e
南宫燚滨 <nangongyibin@gmail.com>
2020-08-10 12:48:10 +0800

    银联支付后端接口书写

---
+++ src/main/java/com/ngyb/payment/MyWebMvcConfigurationSupport.java
@@ -0,0 +1,40 @@
+package com.ngyb.payment;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * 作者：南宫燚滨
+ * 描述：
+ * 邮箱：nangongyibin@gmail.com
+ * 日期：2020/8/10 09:15
+ */
+@Configuration
+public class MyWebMvcConfigurationSupport extends WebMvcConfigurationSupport {
+    /**
+     * 解决中文乱码的问题
+     * @return
+     */
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(responseBodyConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+}
